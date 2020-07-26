e2b4ee4466d19df06de2885fc3874769e2ff659a
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-26 00:27:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+  shoe.condition = "tattered"
+
+  attr_accessor :color, :size, :material
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end 
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+
+end
